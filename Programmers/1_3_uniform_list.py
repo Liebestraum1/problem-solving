465f0a5d2c98a1a0b4f7ed7866e5df8cbdf412fb
Liebestraum1 <psnew14@gmail.com>
2022-11-24 17:22:28 +0900

    1_3_uniform

--- Programmers/1_3_uniform_list.py
+++ Programmers/1_3_uniform_list.py
@@ -1,15 +1,12 @@
 def solution(n, lost, reserve):
-    student = [1] * (n + 2) # +2를 하는 이유는 코드 작성의 편의를 위함
+    student = [1] * (n+2) #2를 더하는 이유는 코드 구현상의 편리함을 위함
     for i in lost:
         student[i] -= 1
     for i in reserve:
         student[i] += 1
-    for i in range(1, n+2):
+    for i in range(1, n+1):
         if student[i] == 2 and student[i-1] == 0:
-            student[i] = 1
-            student[i-1] = 1
+            student[i - 1: i +1] = [1, 1]
         elif student[i] == 2 and student[i+1] == 0:
-            student[i] = 1
-            student[i+1] = 1
-    return student.count(1) + student.count(2) - 2
-
+            student[i: i + 2] = [1, 1]
+    return len([x for x in student[1:-1] if x > 0])
