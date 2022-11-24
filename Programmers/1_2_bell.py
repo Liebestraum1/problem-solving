5e410b6f61c3af00a6d7295a7b4d954b517047b5
Liebestraum1 <psnew14@gmail.com>
2022-11-24 15:26:18 +0900

    1_2_bell

--- Programmers/1_2_bell.py
+++ Programmers/1_2_bell.py
@@ -4,7 +4,7 @@ from itertools import accumulate
 def solution(bell):
     coors_start = {}
     coors_end = {}
-    for i, x in enumerate(accumulate[0] + [-1 if b == 1 else 1 for b in bell]):
+    for i, x in enumerate(accumulate([0] + [-1 if b == 1 else 1 for b in bell])):
         if x not in coors_start:
             coors_start[x] = i
         coors_end[x] = i
