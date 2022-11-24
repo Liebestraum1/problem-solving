d246062c6ca6f1ffc399ce91151b63e34dca9dad
Liebestraum1 <psnew14@gmail.com>
2022-11-24 17:33:38 +0900

    1_3_uniform

--- Programmers/1_3_uniform_hash.py
+++ Programmers/1_3_uniform_hash.py
@@ -1,9 +1,9 @@
 def solution(n, lost, reserve):
     overlap = set(reserve) & set(lost)
     reserve = set(reserve) - overlap
-    lost = list(set(lost) - overlap)
+    lost = set(lost) - overlap
     
-    for i in reserve:
+    for i in sorted(reserve):
         if i - 1 in lost:
             lost.remove(i-1)
         elif i + 1 in lost:
