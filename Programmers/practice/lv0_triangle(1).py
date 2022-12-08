9f89cb600cf7f284ea5689ed735765496dd47527
park_laptop <psnew14@gmail.com>
2022-12-09 02:47:47 +0900

    2022/12/09

--- Programmers/practice/lv0_triangle(1).py
+++ Programmers/practice/lv0_triangle(1).py
@@ -1,3 +1,4 @@
+#Programmers lv.0, 삼각형의 완성조건 1
 def solution(sides):
     if max(sides) >= sum(sides) - max(sides):
         return 2
--- Programmers/practice/lv0_triangle(2).py
+++ Programmers/practice/lv0_triangle(2).py
@@ -1,2 +1,3 @@
+#Programmers lv.0, 삼각형의 완성조건 2
 def solution(sides):
     return min(sides) * 2 - 1
