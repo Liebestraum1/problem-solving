1f8ad05b790e0a79a38a04151689bcbe046f81b9
park_laptop <psnew14@gmail.com>
2023-01-19 19:34:48 +0900

    2023/01/19

--- Programmers/Level_2/가장 큰 수.py
+++ Programmers/Level_2/가장 큰 수.py
@@ -1,4 +1,6 @@
 def solution(numbers):
     numbers = [str(i) for i in numbers]
-    numbers.sort(key = lambda x: int((x*4)[:4]), reverse=True)
+    numbers.sort(key = lambda x: x*3, reverse=True)
     return '0' if int(''.join(numbers)) == 0 else ''.join(numbers)
+
+#아스키 코드에 따라 정렬하기 때문에, line 3에서 int로 변환할 필요가 딱히 없음.
