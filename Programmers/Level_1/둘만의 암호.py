0005181f2980bc137406c41f5cc68889319bd23c
park_laptop <psnew14@gmail.com>
2023-02-11 01:18:12 +0900

    2023/02/11

--- Programmers/Level_1/둘만의 암호.py
+++ Programmers/Level_1/둘만의 암호.py
@@ -1,3 +1,4 @@
+#Sol. 1
 def solution(s, skip, index):
     answer = ''
     for alphabet in s:
@@ -11,3 +12,9 @@ def solution(s, skip, index):
 
 def r(a : int):
     return chr((a - 97) % 26 + 97)
+
+#Sol. 2 -> .index 메소드를 뺄 방법은 없을까?
+def solution(s, skip, index):
+    alphabet = [chr(i) for i in range(97, 123) if chr(i) not in skip]
+    answer = [alphabet[(alphabet.index(a) + index) % len(alphabet)] for a in s]
+    return ''.join(answer)
