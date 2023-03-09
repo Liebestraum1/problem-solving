02bef0520f2b63238318e51e664d7cfbaea67c16
park_laptop <psnew14@gmail.com>
2023-03-09 22:37:31 +0900

    2023/03/09

--- Programmers/Level_2/무인도 여행.py
+++ Programmers/Level_2/무인도 여행.py
@@ -5,21 +5,15 @@ def solution(maps):
     answer = []
     visited = set()
     def dfs(r, c):
-        nonlocal food
-        if r < 0 or r >= len(maps) or c < 0 or c >= len(maps[0]) or maps[r][c] == 'X':
-            return
-        elif (r, c) not in visited:
+        if r < 0 or r >= len(maps) or c < 0 or c >= len(maps[0]) or (r, c) in visited or maps[r][c] == 'X':
+            return 0
+        if (r, c) not in visited:
             visited.add((r, c))
-            food += int(maps[r][c])
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)        
+            return int(maps[r][c]) + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)        
         
     for row in range(len(maps)):
         for col in range(len(maps[0])):
             if maps[row][col] != 'X' and (row, col) not in visited:
-                food = 0
-                dfs(row, col)
-                answer.append(food)
+                answer.append(dfs(row, col))
+    
     return sorted(answer) if answer else [-1]
