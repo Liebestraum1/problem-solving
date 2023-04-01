8e18b71464b9259d24d6642fc54a56292476c641
Liebestraum1 <psnew14@gmail.com>
2023-04-02 04:28:56 +0900

    2023/04/02

--- Programmers/Level_2/과제 진행하기.py
+++ Programmers/Level_2/과제 진행하기.py
@@ -1,13 +1,11 @@
 def solution(plans):
     answer = [] #정답 리스트
-    #1. 배열을 시간순으로 정렬
     for i in range(len(plans)):
         plans[i][1] = int(plans[i][1][:2]) * 60 + int(plans[i][1][3:])
         plans[i][2] = int(plans[i][2])
     
     plans.sort(key = lambda x: x[1], reverse=True)
     
-    #2. 처음 해결해야 할 과제를 뺀다
     name, start, time = plans.pop() #과목명, 시작시간, 소요시간
     remain = []
     
@@ -17,7 +15,6 @@ def solution(plans):
             answer.append(name)
             time_left = ns - (start + time) #남은 시간
             while time_left and remain:
-                print(name, start, time, nn, ns, nt, remain, time_left)
                 if remain[-1][1] <= time_left:
                     time_left -= remain[-1][1]
                     answer.append(remain.pop()[0])
@@ -33,5 +30,3 @@ def solution(plans):
         answer.append(remain.pop()[0])
     
     return answer
-
-print(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]))
