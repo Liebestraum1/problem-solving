7184471cfe4190e41df52400421b35812337aa6e
Liebestraum1 <83405863+Liebestraum1@users.noreply.github.com>
2023-09-26 15:09:17 +0900

    [Gold IV] Title: 스도쿠, Time: 2972 ms, Memory: 153964 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+arr = [list(map(int, list(input().rstrip()))) for _ in range(9)]
+answer = []
+
+def sudoku(arr, row, col):
+    if row == 9:
+        for r in arr:
+            for c in r:
+                print(c, end='')
+            print('')
+        exit()
+    else:
+        if arr[row][col] != 0:
+            sudoku(arr, row + (col+1)//9, (col+1)%9)
+        else:
+            can = arr[row] + [arr[i][col] for i in range(0, 9)] + [i[j] for i in arr[row//3*3:row//3*3+3] for j in range(col//3*3, col//3*3+3)]
+            for num in range(1, 10):
+                if num not in can:
+                    arr[row][col] = num
+                    sudoku(arr, row + (col+1)//9, (col+1)%9)
+                    arr[row][col] = 0
+sudoku(arr, 0, 0)
