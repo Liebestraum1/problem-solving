2e60e7d8c7902d3683fec3ece6bb1d5a309e7d5f
Liebestraum1 <83405863+Liebestraum1@users.noreply.github.com>
2023-12-13 22:29:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.47 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,8 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String[] strings = s.split(" ");
-        int maxInt = Arrays.stream(strings).mapToInt(Integer :: parseInt).max().getAsInt();
-        int minInt = Arrays.stream(strings).mapToInt(Integer :: parseInt).min().getAsInt();
-        return minInt + " " + maxInt;
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        
+        for(String parsedString : strings){
+            int curValue = Integer.parseInt(parsedString);
+            if(curValue >= maxValue)
+                maxValue = curValue;
+            if(curValue <= minValue)
+                minValue = curValue;
+        }
+        return minValue + " " + maxValue;
     }
 }
