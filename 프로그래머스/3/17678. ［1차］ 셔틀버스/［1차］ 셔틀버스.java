4759a756d4c349a75e36f6c6b6011c2fbf850a5b
Liebestraum1 <83405863+Liebestraum1@users.noreply.github.com>
2024-02-19 22:26:22 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 5.70 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -9,63 +9,39 @@ class Solution {
     }
     
     public String timeBuilder(int time){
-        StringBuilder sb = new StringBuilder();
-        
         int hour = time / 60;
         int minute = time % 60;
-        
-        if(hour < 10) {
-            sb.append(0).append(hour);
-        } else {
-            sb.append(hour);
-        }
-        
-        if(minute < 10){
-            sb.append(":").append(0).append(minute);
-        } else {
-            sb.append(":").append(minute);
-        }
-        
-        return sb.toString();
+        return String.format("%02d:%02d", hour, minute);
     }
     
     public String solution(int n, int t, int m, String[] timetable) {
         int answer = 0;
-        Queue<Integer> queue = new ArrayDeque<>();
-        int[] intTimetable = new int[timetable.length];
+        int index = 0;
+        final int SIZE = timetable.length;
+        int[] intTimetable = new int[SIZE];
         
-        for(int i = 0; i < timetable.length; i++){
+        for(int i = 0; i < SIZE; i++){
             intTimetable[i] = timeParser(timetable[i]);
         }
         
         Arrays.sort(intTimetable);
-        
-        for(int i = 0; i < timetable.length; i++){
-            queue.add(intTimetable[i]);
-        }
         
         for(int i = 0; i < n; i++){
             int startTime = 540 + i * t;
             int lastPassenger = 1440;
             int count = 0;
             
-            while(!queue.isEmpty() && count < m){
-                lastPassenger = queue.peek();
+            while(index < SIZE && count < m){
+                lastPassenger = intTimetable[index];
                 if(lastPassenger <= startTime){
-                    queue.poll();
+                    index += 1;
                     count += 1;
                 } else {
                     break;
                 } 
             }
-            
-            if(count == m){
-                answer = lastPassenger - 1;
-            } else {
-                answer = startTime;
-            }
+            answer = count == m ? lastPassenger - 1 : startTime;
         }
-        
         return timeBuilder(answer);
     }
 }
